109282cc81a9cd8da8cedba65ffa181ba5ef6f6c
AgielF <agielnanda2004@gmail.com>
2023-12-08 13:11:00 +0700

    first commit

---
+++ resources/views/daftar.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <h1>ini page daftar</h1>
+    <script src="https://unpkg.com/typeit@@{TYPEIT_VERSION}/dist/index.umd.js"></script>
+    <script src="{{ asset('js/daftar.js') }}"></script>
+</body>
+</html>
