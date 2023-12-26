f9b1e232ae80417195e6c67fa20d0ddbee3a65c1
Bagus Anugrah <bagus.anugrah71@gmail.com>
2023-12-26 14:59:07 +0700

    beberapa perubahan pada html daftar

--- resources/views/layouts/daftar.blade.php
+++ resources/views/layouts/daftar.blade.php
@@ -29,7 +29,7 @@
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card text-dark" style="border-radius: 1rem;" id="background-card">
           <div class="card-body p-5 text-center">
-
+            <form action="/daftar" method="post">
             <div class="mb-md-5 mt-md-4">
 
               <h2 class="fw-bold mb-4 text-uppercase text-white">Daftar</h2>
@@ -62,6 +62,7 @@
               </div>
               <button class="btn btn-outline-light btn-lg px-5" type="submit">Daftar</button>
             </div>
+            </form>
           </div>
         </div>
       </div>
