38c091994dda5a4da571ebd2e69a4d7dcceaac04
Bagus Anugrah <bagus.anugrah71@gmail.com>
2023-12-26 14:48:09 +0700

    perubahan tampilan daftar

--- resources/views/layouts/daftar.blade.php
+++ resources/views/layouts/daftar.blade.php
@@ -8,9 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body id="all">
-    <div class="wrapper">
+    {{-- <div class="wrapper">
         <h1>SELAMAT DATANG DI BAGUS MOTOR</h1>
-    </div>
+    </div> --}}
     
     <section class="wrapper-footer">
     <div class="card" style="width: 48rem; border-radius: 1rem;" id="background-card" >
@@ -20,41 +20,48 @@
        
       </div>
     </div>
-</section>
+    </section>
 
 
-    <section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
+<section class="vh-100 gradient-custom">
+  <div class="container py-5">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card text-dark" style="border-radius: 1rem;" id="background-card">
           <div class="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+            <div class="mb-md-5 mt-md-4">
 
-              <h2 class="fw-bold mb-2 text-uppercase text-white">SIGN IN</h2>
-
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label text-white" for="typeEmailX">Email</label>
+              <h2 class="fw-bold mb-4 text-uppercase text-white">Daftar</h2>
+              <div class="mb-5">
+                <select name="role" class="form-select" aria-label="Default select example" required>
+                  <option value="">-- Daftar Sebagai --</option>
+                  <option value="pemilik">Pemilik Motor</option>
+                  <option value="penyewa">Penyewa Motor</option>
+                </select>
               </div>
-
               <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                <label class="form-label text-white" for="typePasswordX">Password</label>
+                <input type="text" id="nik" name="nik" class="form-control form-control-lg" />
+                <label class="form-label text-white" for="nik">NIK</label>
               </div>
-              <select name="role" class="form-select" aria-label="Default select example" required>
-								<option value="">-- Pilih Salah Satu --</option>
-								<!-- <option value="admin">Admin</option> -->
-								<option value="pemilik">Pemilik Motor</option>
-								<option value="penyewa">Penyewa Motor</option>
-							</select>
-
-              <button class="btn btn-outline-light btn-lg px-5" type="submit">SIGN IN</button>
-
+              <div class="form-outline form-white mb-4">
+                <input type="text" id="nama" name="nama" class="form-control form-control-lg" />
+                <label class="form-label text-white" for="nama">Nama</label>
+              </div>
+              <div class="form-outline form-white mb-4">
+                <input type="text" id="nohp" name="nohp" class="form-control form-control-lg" />
+                <label class="form-label text-white" for="nohp">No. HP</label>
+              </div>
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="username" name="username" class="form-control form-control-lg" />
+                <label class="form-label text-white" for="username">Username</label>
+              </div>
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="password" class="form-control form-control-lg" />
+                <label class="form-label text-white" for="password">Password</label>
+              </div>
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Daftar</button>
             </div>
-
-
           </div>
         </div>
       </div>
@@ -65,13 +72,13 @@
 
 
 <section class="wrapper-footer">
-    <div class="card" style="width: 48rem; border-radius: 1rem;" id="background-card">
+    {{-- <div class="card" style="width: 48rem; border-radius: 1rem;" id="background-card">
       <div class="card-body">
         <h5 class="card-title text-center">🌟Kenapa Memilih Rental Motor Kami?</h5>
         <p class="card-text">✅ Fasilitas Terbaik: Motor-motor kami selalu dalam kondisi prima dan siap melibas jalanan dengan performa maksimal.</p>
         <p class="card-text">✅ Harga Terjangkau: Nikmati petualangan tanpa harus menguras kantong. Harga sewa motor kami sangat bersaing dan cocok untuk semua budget.</p>
       </div>
-    </div>
+    </div> --}}
 </section>
 
 <div>
